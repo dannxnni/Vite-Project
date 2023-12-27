5598b61ebb06d26a02326ffa764af1faff6cae87
dannxnni <132893321+dannxnni@users.noreply.github.com>
2023-12-27 16:28:28 +0900

    Add files via upload
    Detail 페이지 추가

---
+++ vite/src/pages/detail/detail.js
@@ -0,0 +1,20 @@
+import {setDocumentTitle} from "/src/lib"
+import "/src/pages/detail/detail.css"
+import pb from "/src/api/pocketbase";
+
+setDocumentTitle("2.9cm / Detail");
+
+async function renderProductData(){
+
+  const hash = window.location.hash.slice(1);
+
+  const productData = await pb
+  .collection('products')
+  .getOne(hash)
+  
+
+  console.log( productData );
+
+}
+
+renderProductData();
