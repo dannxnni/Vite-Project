04bf8b216c2908cee3ce264e44359a64bec50423
dannxnni <132893321+dannxnni@users.noreply.github.com>
2023-12-27 16:30:29 +0900

    main.js 조건부 랜더링 추가

--- vite/main.js
+++ vite/main.js
@@ -1,5 +1,7 @@
 import "/src/styles/style.css"
 import gsap from "gsap"
+import { getStorage, insertLast, getNode, deleteStorage } from "/src/lib/";
+import pb from "/src/api/pocketbase";
 
 const tl = gsap.timeline();
 
@@ -12,3 +14,28 @@ tl.from(".visual", {
   x: -30,
   stagger: 0.2
 })
+
+if(localStorage.getItem('auth')){
+  const {isAuth,user} = await getStorage('auth');
+
+  if(isAuth){
+    const template = /* html */`
+      <div class="userName">${user.name}님 반갑습니다</div>
+      <button class="logout" type="button">로그아웃</button>
+    `
+    insertLast('.container',template)
+  }
+}
+
+
+const logout = getNode(".logout");
+
+if (logout) {
+
+  logout.addEventListener("click", () => {
+    pb.authStore.clear();   // local storage에서 pocketbase 삭제
+    deleteStorage("auth");  // local storage에서 auth 삭제
+    window.location.reload(); // logout 버튼 사라져
+  });
+
+}
